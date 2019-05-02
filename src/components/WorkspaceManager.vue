b336c8bfe0b6cd21c8cc9580330fe17b05b2ec24
I348307 <doron.nahari@sap.com>
2019-05-02 15:07:31 +0300

    init

---
+++ src/components/WorkspaceManager.vue
@@ -0,0 +1,20 @@
+<template>
+  <b-container fluid>
+    <b-table striped hover :items="items"></b-table>
+  </b-container>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        items: [
+          { Status: 40, Name: 'Dickerson', Id: 'Macdonald', Actions: 'Icon' },
+          { Status: 21, Name: 'Larsen', Id: 'Shaw', Actions: 'Icon' },
+          { Status: 89, Name: 'Geneva', Id: 'Wilson', Actions: 'Icon' },
+          { Status: 38, Name: 'Jami', Id: 'Carney', Actions: 'Icon' }
+        ]
+      }
+    }
+  }
+</script>
